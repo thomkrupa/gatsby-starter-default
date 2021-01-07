3e8948ac95dbad38db05a3b1fdab00b78572081a
Thom Krupa <thom@bejamas.io>
2021-01-07 18:30:03 +0100

    feat: add gatsby-plugin-image

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,112 @@
  */
 
 // You can delete this file if you're not using it
+
+const { resolve, relative } = require(`path`)
+
+const { ensureDir, readdir, copy } = require(`fs-extra`)
+
+async function calculateDirs(
+  store,
+  { extraDirsToCache = [], cachePublic = false }
+) {
+  const program = store.getState().program
+  const rootDirectory = program.directory
+
+  const dirsToCache = [
+    cachePublic && resolve(rootDirectory, `public/static`),
+    cachePublic && resolve(rootDirectory, `public/page-data`),
+    ...extraDirsToCache.map(dirToCache => resolve(rootDirectory, dirToCache)),
+  ].filter(Boolean)
+
+  for (const dir of dirsToCache) {
+    await ensureDir(dir)
+  }
+
+  const nowCacheDir = resolve(rootDirectory, `../.cache`, `gatsby`)
+
+  await ensureDir(nowCacheDir)
+
+  return {
+    rootDirectory,
+    dirsToCache,
+    nowCacheDir,
+  }
+}
+
+function generateCacheDirectoryNames(rootDirectory, nowCacheDir, dirPath) {
+  const relativePath = relative(rootDirectory, dirPath)
+  const dirName = relativePath.replace("/", "--")
+  const cachePath = resolve(nowCacheDir, dirName)
+  const humanName = relativePath
+  return { cachePath, humanName }
+}
+
+exports.onPreInit = async function(
+  { store },
+  { extraDirsToCache, cachePublic }
+) {
+  // if (!process.env.now_BUILD_BASE) {
+  //   return
+  // }
+
+  const { dirsToCache, nowCacheDir, rootDirectory } = await calculateDirs(
+    store,
+    {
+      extraDirsToCache,
+      cachePublic,
+    }
+  )
+
+  for (const dirPath of dirsToCache) {
+    const { cachePath, humanName } = generateCacheDirectoryNames(
+      rootDirectory,
+      nowCacheDir,
+      dirPath
+    )
+
+    await ensureDir(cachePath)
+
+    const dirFiles = await readdir(dirPath)
+    const cacheFiles = await readdir(cachePath)
+
+    console.log(
+      `plugin-now-cache: Restoring ${cacheFiles.length} cached files for ${humanName} directory with ${dirFiles.length} already existing files.`
+    )
+
+    await copy(cachePath, dirPath)
+  }
+
+  console.log(`plugin-now-cache: ZEIT Now cache restored`)
+}
+
+exports.onPostBuild = async function(
+  { store },
+  { extraDirsToCache, cachePublic }
+) {
+  // if (!process.env.now_BUILD_BASE) {
+  //   return
+  // }
+
+  const { dirsToCache, nowCacheDir, rootDirectory } = await calculateDirs(
+    store,
+    {
+      extraDirsToCache,
+      cachePublic,
+    }
+  )
+
+  for (const dirPath of dirsToCache) {
+    const { cachePath, humanName } = generateCacheDirectoryNames(
+      rootDirectory,
+      nowCacheDir,
+      dirPath
+    )
+
+    console.log(`plugin-now-cache: Caching ${humanName}...`)
+
+    await copy(dirPath, cachePath)
+  }
+
+  console.log(`plugin-now-cache: ZEIT Now cache refilled`)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {data.allFile.edges.map(({ node }) => (
+      <div key={node.id}>
+        <GatsbyImage image={getImage(node)} alt="my gatsby image" />
+      </div>
+    ))}
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            gatsbyImageData(maxWidth: 800)
+          }
+        }
+      }
+    }
+  }
+`
